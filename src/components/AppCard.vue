5a1095ec2d991a869a72cdbf542d06d8b481f5a1
MarinaLasorsa <marina.lasorsa@gmail.com>
2024-04-11 16:26:02 +0200

    ADD: template and style in card component + imported card component in main component, cycled with a v-for

--- src/components/AppCard.vue
+++ src/components/AppCard.vue
@@ -2,8 +2,89 @@
 </script>
 
 <template>
-
+    <div class="col-1-3">
+        <div class="card-header">
+            <img src="/1.webp" alt="">
+            <span class="heart-badge">&hearts;</span>
+            <div>
+                <span class="badge badge-green">Sostenibilità</span>
+                <span class="badge badge-red">-50%</span>
+            </div>
+            <img src="/1b.webp" class="hover-image">
+        </div>
+        <div class="card-body">
+            <p class="text-small">Levi's</p>
+            <h5>RELAXED FIT TEE UNISEX</h5>
+            <p> <span class="text-small price">14,99 &euro; </span><span class="text-small line-through">29,99 &euro;</span></p>
+        </div>
+    </div>
 </template>
 
 <style scoped>
+    /*CARD*/
+
+    .card-header {
+        position: relative;
+    }
+
+    .card-body .text-small {
+        font-size: 12px;
+    }
+
+    .price {
+        font-weight: bold;
+        color: red;
+    }
+
+    .hover-image {
+        position: absolute;
+        top: 0;
+        left: 0;
+        opacity: 0
+    }
+
+    .col-1-3:hover .hover-image {
+        opacity: 1
+    }
+
+    /*COMPONENTS*/
+
+    .heart-badge {
+        background-color: rgb(255, 255, 255);
+        font-size: 30px;
+        padding: 0 10px;
+        position: absolute;
+        right: 0;
+        top: 10px;
+        z-index: 999;
+    }
+
+    .badge {
+        color: white;
+        font-size: 10px;
+        padding: 5px;
+        font-weight: bold;
+        z-index: 999;
+
+    }
+
+    .badge.badge-green {
+        background-color: green;
+        position: absolute;
+        left: 35px;
+        bottom: 35px;
+    }
+
+    .badge.badge-red {
+        background-color: red;
+        position: absolute;
+        left: 0;
+        bottom: 35px;
+    }
+
+    .not-on-sale .badge.badge-green {
+        position: absolute;
+        left: 0px;
+        bottom: 35px;
+    }
 </style>
--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,27 +1,127 @@
 <script>
+    import AppCard from './AppCard.vue'
+
+    export default {
+        components: {
+            AppCard
+        },
+        data () {
+            return {
+                products: [
+                    {
+                    "id": 1,
+                    "frontImage": "1.webp",
+                    "backImage": "1b.webp",
+                    "brand": "Levi's",
+                    "name": "Relaxed Fit",
+                    "price": 29.99,
+                    "isInFavorites": true,
+                    "badges": [
+                        {
+                        "type": "tag",
+                        "value": "Sostenibilità"
+                        },
+                        {
+                        "type": "discount",
+                        "value": "-50%"
+                        }
+                    ]
+                    },
+                    {
+                    "id": 2,
+                    "frontImage": "2.webp",
+                    "backImage": "2b.webp",
+                    "brand": "Guess",
+                    "name": "Roses Tee",
+                    "price": 20.99,
+                    "isInFavorites": true,
+                    "badges": [
+                        {
+                        "type": "discount",
+                        "value": "-30%"
+                        }
+                    ]
+                    },
+                    {
+                    "id": 3,
+                    "frontImage": "3.webp",
+                    "backImage": "3b.webp",
+                    "brand": "Come Zucchero Filato",
+                    "name": "Voglia di colori pastello",
+                    "price": 129.99,
+                    "isInFavorites": false,
+                    "badges": [
+                        {
+                        "type": "discount",
+                        "value": "-30%"
+                        }
+                    ]
+                    },
+                    {
+                    "id": 4,
+                    "frontImage": "4.webp",
+                    "backImage": "4b.webp",
+                    "brand": "Levi's",
+                    "name": "Tee Unisex",
+                    "price": 14.99,
+                    "isInFavorites": false,
+                    "badges": [
+                        {
+                        "type": "tag",
+                        "value": "Sostenibilità"
+                        },
+                        {
+                        "type": "discount",
+                        "value": "-50%"
+                        }
+                    ]
+                    },
+                    {
+                    "id": 5,
+                    "frontImage": "5.webp",
+                    "backImage": "5b.webp",
+                    "brand": "Maya Deluxe",
+                    "name": "Stripe Bodice",
+                    "price": 99.99,
+                    "isInFavorites": true,
+                    "badges": [
+                        {
+                        "type": "tag",
+                        "value": "Sostenibilità"
+                        },
+                        {
+                        "type": "discount",
+                        "value": "-50%"
+                        }
+                    ]
+                    },
+                    {
+                    "id": 6,
+                    "frontImage": "6.webp",
+                    "backImage": "6b.webp",
+                    "brand": "Esprit",
+                    "name": "Maglione - Black",
+                    "price": 29.99,
+                    "isInFavorites": true,
+                    "badges": [
+                        {
+                        "type": "tag",
+                        "value": "Sostenibilità"
+                        }
+                    ]
+                    }
+                ]
+            }
+        }
+    }
 </script>
 
 <template>
     <main class="page-content">
         <div class="container"> 
             <div class="row"> 
-                <div class="col-1-3">
-                    <div class="card-header">
-                        <img src="/1.webp" alt="">
-                        <span class="heart-badge">&hearts;</span>
-                        <div>
-                            <span class="badge badge-green">Sostenibilità</span>
-                            <span class="badge badge-red">-50%</span>
-                        </div>
-                        <img src="/1b.webp" class="hover-image">
-                    </div>
-                    <div class="card-body">
-                        <p class="text-small">Levi's</p>
-                        <h5>RELAXED FIT TEE UNISEX</h5>
-                        <p> <span class="text-small price">14,99 &euro; </span><span class="text-small line-through">29,99 &euro;</span></p>
-                    </div>
-                </div>
-                <div class="col-1-3">
+                <AppCard v-for="(card, i) in products" :key="i"/>
+                <!--<div class="col-1-3">
                     <div class="card-header">
                         <img src="/2.webp" alt="">
                         <span class="heart-badge">&hearts;</span>
@@ -93,7 +193,7 @@
                         <h5>MAGLIONE - BLACK</h5>
                         <p> <span class="text-small price">29,99 &euro; </span></p>
                     </div>
-                </div>
+                </div>-->
             </div>
         </div>
     </main>
@@ -101,70 +201,5 @@
 
 <style scoped>
 
-    /*CARD*/
-
-    .card-header {
-        position: relative;
-    }
-
-    .card-body .text-small {
-        font-size: 12px;
-    }
-
-    .price {
-        font-weight: bold;
-        color: red;
-    }
-
-    .hover-image {
-        position: absolute;
-        top: 0;
-        left: 0;
-        opacity: 0
-    }
-
-    .col-1-3:hover .hover-image {
-        opacity: 1
-    }
-
-    /*COMPONENTS*/
-
-    .heart-badge {
-        background-color: rgb(255, 255, 255);
-        font-size: 30px;
-        padding: 0 10px;
-        position: absolute;
-        right: 0;
-        top: 10px;
-        z-index: 999;
-    }
-
-    .badge {
-        color: white;
-        font-size: 10px;
-        padding: 5px;
-        font-weight: bold;
-        z-index: 999;
-
-    }
-
-    .badge.badge-green {
-        background-color: green;
-        position: absolute;
-        left: 35px;
-        bottom: 35px;
-    }
-
-    .badge.badge-red {
-        background-color: red;
-        position: absolute;
-        left: 0;
-        bottom: 35px;
-    }
-
-    .not-on-sale .badge.badge-green {
-        position: absolute;
-        left: 0px;
-        bottom: 35px;
-    }
+    
 </style>
